82bfabf92e0f520aec8b9d0654db612c08346619
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-30 16:42:51 +0200

    EDIT bonus 1 (add more film type )

---
+++ BONUS/index.php
@@ -0,0 +1,37 @@
+<?php
+require_once "./Movie.php";
+
+$movie1 = new Movie ("The Witch",["Horror","Storic"]);
+$movie1->releseYear = 2015;
+$movie1->ibdmScore = 6.9;
+
+var_dump($movie1);
+var_dump($movie1->getParentalAdv());
+
+$movie2 = new Movie ("Sin City",["Thriller","Noir"]);
+$movie2->releseYear = 2005;
+$movie2->ibdmScore = 8.0;
+
+var_dump($movie2);
+var_dump($movie2->getParentalAdv());
+
+$movie3 = new Movie ("Finding Nemo","Animation");
+$movie3->releseYear = 2003;
+$movie3->ibdmScore = 8.2;
+
+var_dump($movie3);
+var_dump($movie3->getParentalAdv());
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 1</title>
+</head>
+<body>
+    
+</body>
+</html>
