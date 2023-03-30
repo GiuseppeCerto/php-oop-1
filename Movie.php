bb6f8d617225bd865bf1188e7b8d6fb551208d70
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-03-30 16:12:39 +0200

    EDIT movie class with movie properties

---
+++ Movie.php
@@ -0,0 +1,31 @@
+<?php
+
+class Movie {
+    public string $title;
+    public string $filmType;
+    public int $releseYear;
+    public float $ibdmScore;
+
+    function __construct($_title, $_filmType) {
+        $this->title = $_title;
+        $this->filmType = $_filmType;
+    }
+
+    function getParentalAdv(){
+        if($this->filmType === "Horror")
+
+        return "Forbidden to children under 18 years old";
+
+        elseif($this->filmType === "Thriller")
+
+        return "Forbidden to children under 14 years old";
+
+        else
+
+        return "Movies for everyone";
+    }
+
+
+}
+
+?>
